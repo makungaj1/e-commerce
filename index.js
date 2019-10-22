9ce0eba77a228403cd70214f463e25130082088a
jonathan <myemail@bbc.com>
2019-10-22 19:50:42 -0400

    index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const error = require('./middleware/error');
+app.use(express.json());
+require('./config/db')();
+
+app.use('/api/employees', require('./routes/employees'));
+app.use('/api/customers', require('./routes/customers'));
+app.use('/api/products', require('./routes/products'));
+app.use('/api/inventories', require('./routes/inentories'));
+app.use('/api/auth', require('./routes/auth'));
+
+// error handeling
+// app.use(error);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
